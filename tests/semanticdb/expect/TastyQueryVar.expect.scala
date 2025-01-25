618ccc85aa078b050b56efc414d86e7e208dbd11
Matthieu <matthieu.saurylericoux@gmail.com>
2025-01-25 21:17:12 +0100

    produce SDB of var reassigning

--- tests/semanticdb/expect/TastyQueryVar.expect.scala
+++ tests/semanticdb/expect/TastyQueryVar.expect.scala
@@ -1,8 +1,10 @@
 class TastyQueryVar/*<-_empty_::TastyQueryVar#*/{
-    var v/*<-_empty_::TastyQueryVar#v().*/ = 1
-    val x/*<-_empty_::TastyQueryVar#x.*/ = v/*->_empty_::TastyQueryVar#v().*/
-    def foo/*<-_empty_::TastyQueryVar#foo().*/(x/*<-_empty_::TastyQueryVar#foo().(x)*/ : Int/*->scala::Int#*/) = {
-        var z/*<-local0*/ = "Hello"
-    }
+    var x/*<-_empty_::TastyQueryVar#x().*/ = 2
+    x/*->_empty_::TastyQueryVar#`x_=`().*/ = 3
 
+    def foo/*<-_empty_::TastyQueryVar#foo().*/ =
+        var z/*<-local0*/ = 2
+        z/*->local0*/ = 3
+    var p/*<-_empty_::TastyQueryVar#p().*/ = foo/*->_empty_::TastyQueryVar#foo().*/
+    p/*->_empty_::TastyQueryVar#`p_=`().*/ = {val x/*<-local1*/ = true}
 }
--- tests/semanticdb/expect/TastyQueryVar.scala
+++ tests/semanticdb/expect/TastyQueryVar.scala
@@ -1,9 +1,10 @@
 class TastyQueryVar{
-    var v = 1
-    val x = v
-    
-    def foo(x : Int) = {
-        var z = "Hello"
-    }
+    var x = 2
+    x = 3
 
+    def foo = 
+        var x = 2
+        x = 3
+    var p = foo
+    p = {val x = true}
 }
